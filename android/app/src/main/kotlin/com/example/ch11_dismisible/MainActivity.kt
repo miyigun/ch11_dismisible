7db9eee42ba90314035385e0429c6a6cf00c146b
Murat Iyigun <miyigun@hotmail.com>
2023-04-09 16:04:23 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ch11_dismisible/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ch11_dismisible
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:ch11_dismisible/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dismissible Example',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  const Home(),
+    );
+  }
+}
